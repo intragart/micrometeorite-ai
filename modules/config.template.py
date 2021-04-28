0b244ae7ef6258161d83b08719edf4f8e139b401
Marco Weingart <weingart.marco@hotmail.de>
2021-04-28 23:00:25 +0200

    Added script to test optimizers

--- modules/config.template.py
+++ modules/config.template.py
@@ -5,3 +5,4 @@ TEST_DATA = "" # root directory for test data
 MODEL_PATH = "" # path where the created model should be saved to
 IMG_SIZE = 30 # image size in pixels (IMG_SIZExIMG_SIZE) to train the model
 TRAINING_EPOCHS = 10 # how many training epochs for the model training
+PERCENT_TEST = 0.25 # TODO: ---------- DESC HERE ----------
