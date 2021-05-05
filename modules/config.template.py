0849bf836fb675667374711edfa22e201606e184
Marco Weingart <weingart.marco@hotmail.de>
2021-05-05 18:11:14 +0200

    added comments

--- modules/config.template.py
+++ modules/config.template.py
@@ -1,9 +1,39 @@
-TRAINED_MODEL = "" # path to model which is used to predict the given data
-PREDICT_DATA = "" # path to the data which should be used on the model
-PREDICT_RESULTS = "" # path where predictions should be saved
-TRAINING_DATA = "" # root directory for training data
-TEST_DATA = "" # root directory for test data
-MODEL_PATH = "" # path where the created model should be saved to
-IMG_SIZE = 30 # image size in pixels (IMG_SIZExIMG_SIZE) to train the model
-TRAINING_EPOCHS = 10 # how many training epochs for the model training
-PERCENT_TEST = 0.25 # TODO: ---------- DESC HERE ----------
+# path to the model which is used to predict new data
+TRAINED_MODEL = ""
+
+# path to the data to be predicted by the model
+# can be directory or file
+PREDICT_DATA = ""
+
+# path where predictions for new data should be saved
+# TODO: nothing is saved if empty
+PREDICT_RESULTS = ""
+
+# path to training data (root-dir)
+# the root dir needs subdirs with the labels and the corresponding data inside
+# of them
+# /root-dir
+#    - /label_1
+#    - /label_2
+TRAINING_DATA = ""
+
+# path to test (verification) data (root-dir)
+# the root dir needs subdirs with the labels and the corresponding data inside
+# of them
+# /root-dir
+#    - /label_1
+#    - /label_2
+TEST_DATA = ""
+
+# path where the crated model should be saved to
+MODEL_PATH = ""
+
+# image size in pixels to train the model with
+# all images are resized to IMG_SIZExIMG_SIZE
+IMG_SIZE = 30
+
+# specifies the amount of trainig epochs for the model
+TRAINING_EPOCHS = 10
+
+# TODO: DESC HERE
+PERCENT_TEST = 0.25
--- modules/zip_dir.py
+++ modules/zip_dir.py
@@ -1,22 +1,47 @@
+# import libraries
 import os
 import zipfile
 
 def zip_dir(zip_file, dir_to_zip, zip_handle=None, subpath=""):
-    
+    """Creates a Zipfile of the given directory and all subdirectories
+
+    Keyword arguments:
+    zip_file -- path+filename where the zipfile should be saved
+    dir_to_zip -- path to be zipped
+    zip_handle -- used by function to pass on the ziphandle into recursive calls
+    subpath -- used by function to build the filetree for recursive calls
+    """
+
+    # this checks if there's already a ziphandle given. if no handle is given
+    # the function hasn't been recursively called by itself
     if zip_handle is None:
-        # first instance
+        # first instance - not recursively called
+        # save the current working dir to be able to change back to it when
+        # function is finished
         main_cwd = os.getcwd()
+
+        # set current working dir to the directory that should be zipped
         os.chdir(dir_to_zip)
+
+        # create a ziphandle and call the function recursive using the 
+        # filehandle starting with the "root" directory 
         with zipfile.ZipFile(zip_file, "w") as zf:
             zip_dir("", dir_to_zip+"/", zf)
+
+        # change the current working dir back to the original working dir
         os.chdir(main_cwd)
     else:
-        # n-th instance
+        # n-th instance of function
         # loop through each object in current dir
         for entry in os.listdir(dir_to_zip+subpath):
 
+            # building a path+file-string for the current object of the
+            # directory
             obj = dir_to_zip+subpath+entry
 
+            # If the current object is a directory write it in the zipfile and
+            # call the function recursively
+            # If the current object is a file write it into the zipfile
             if os.path.isdir(obj):
                 zip_handle.write(subpath+entry)
                 zip_dir("", dir_to_zip, zip_handle, subpath+entry+"/")
