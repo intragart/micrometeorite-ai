113fa5b9f24783cf6cc5f8a465223e22a9871a46
Marco Weingart <weingart.marco@hotmail.de>
2021-07-21 21:18:51 +0200

    finalized config.template.py

--- modules/config.template.py
+++ modules/config.template.py
@@ -1,3 +1,5 @@
+#---------------------------- Predicting Models --------------------------------
+
 # path to the model which is used to predict new data
 TRAINED_MODEL = ""
 
@@ -6,8 +8,10 @@ TRAINED_MODEL = ""
 PREDICT_DATA = ""
 
 # path where predictions for new data should be saved
-# TODO: nothing is saved if empty
+# nothing is saved if empty
 PREDICT_RESULTS = ""
+
+#---------------------------- Training Models ----------------------------------
 
 # path to training data (root-dir)
 # the root dir needs subdirs with the labels and the corresponding data inside
@@ -17,6 +21,33 @@ PREDICT_RESULTS = ""
 #    - /label_2
 TRAINING_DATA = ""
 
+# path where the crated model should be saved to
+MODEL_PATH = ""
+
+# path to raw data (root-dir)
+# the root dir needs subdirs with the labels and the corresponding data inside
+# of them
+# /root-dir
+#    - /label_1
+#    - /label_2
+RAW_DATA = ""
+
+# image size in pixels to train the model with
+# all images are resized to IMG_SIZExIMG_SIZE
+IMG_SIZE = 141
+
+# specifies the amount of trainig epochs for the model
+TRAINING_EPOCHS = 30
+
+# sets the percentage of training data which is not used for training but for validation
+PERCENT_TEST = 0.20
+
+#---------------------------- Other --------------------------------------------
+
+# path to the model used for contour detection (prepareImage.ipynb)
+CONTOUR_MODEL = ""
+
+# !! not used by the main scripts !!
 # path to test (verification) data (root-dir)
 # the root dir needs subdirs with the labels and the corresponding data inside
 # of them
@@ -24,16 +55,3 @@ TRAINING_DATA = ""
 #    - /label_1
 #    - /label_2
 TEST_DATA = ""
-
-# path where the crated model should be saved to
-MODEL_PATH = ""
-
-# image size in pixels to train the model with
-# all images are resized to IMG_SIZExIMG_SIZE
-IMG_SIZE = 30
-
-# specifies the amount of trainig epochs for the model
-TRAINING_EPOCHS = 10
-
-# TODO: DESC HERE
-PERCENT_TEST = 0.25
